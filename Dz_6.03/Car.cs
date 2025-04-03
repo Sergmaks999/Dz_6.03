b0d88104e27fde904beae24a98f3277f896ea3f6
Top <Top@8-12>
2025-04-03 13:18:53 +0500

    Добавьте файлы проекта.

---
+++ Dz_6.03/Car.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public struct Car : ITransport
+{
+    public string LicensePlate { get; set; }
+    public string Brand { get; set; }
+    public string Model { get; set; }
+    public string Color { get; set; }
+
+    public static string TransportType => "Автомобиль";
+    public double RentPricePerMinute { get; set; }
+
+    public Car(string licensePlate, string brand, string model, string color, double rentPricePerMinute)
+    {
+        LicensePlate = licensePlate;
+        Brand = brand;
+        Model = model;
+        Color = color;
+        RentPricePerMinute = rentPricePerMinute;
+    }
+}
---
+++ Dz_6.03/Scooter.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public struct Scooter : ITransport
+{
+    public string Model { get; set; }
+    public int Year { get; set; }
+
+    public static string TransportType => "Самокат";
+    public double RentPricePerMinute { get; set; }
+
+    public Scooter(string model, int year, double rentPricePerMinute)
+    {
+        Model = model;
+        Year = year;
+        RentPricePerMinute = rentPricePerMinute;
+    }
+}
