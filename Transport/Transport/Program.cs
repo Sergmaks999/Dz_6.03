9785a084a4a400f81ab92a232f02235beb48d67e
maks <sergeevmaks444@gmail.com>
2025-04-11 17:30:57 +0500

    Add files via upload

---
+++ Transport/Transport/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.ConstrainedExecution;
+
+public class TransportInfo
+{
+    public static void Main(string[] args)
+    {
+        Scooter scooter = new Scooter("Xiaomi Mijia M365", 2021, 5);
+        TransportRental<Scooter> scooterRental = new TransportRental<Scooter>("Иванов Иван Иванович", scooter, 30);
+
+        Console.WriteLine(scooterRental.GetRentalInfo());
+        Console.WriteLine($"Полная стоимость аренды: {scooterRental.CalculateTotalCost()} руб.");
+
+        Car car = new Car("А123BC777", "Lada", "Vesta", "Белый", 15);
+        TransportRental<Car> carRental = new TransportRental<Car>("Петров Петр Петрович", car, 60);
+
+        Console.WriteLine(carRental.GetRentalInfo());
+        Console.WriteLine($"Полная стоимость аренды: {carRental.CalculateTotalCost()} руб.");
+    }
+}
---
+++ Transport/Transport/Rental.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class TransportRental<T> where T : ITransport
+{
+    public string ClientFIO { get; set; }
+    public T Transport { get; set; }
+    public int RentalTimeMinutes { get; set; }
+
+    public TransportRental(string clientFIO, T transport, int rentalTimeMinutes)
+    {
+        ClientFIO = clientFIO;
+        Transport = transport;
+        RentalTimeMinutes = rentalTimeMinutes;
+    }
+
+    public string GetRentalInfo()
+    {
+        string transportInfo = "";
+
+        if (Transport is Scooter scooter)
+        {
+            transportInfo = $"Тип: {Scooter.TransportType}, Модель: {scooter.Model}, Год выпуска: {scooter.Year}, Цена за минуту: {scooter.RentPricePerMinute}";
+        }
+        else if (Transport is Car car)
+        {
+            transportInfo = $"Тип: {Car.TransportType}, Гос.номер: {car.LicensePlate}, Марка: {car.Brand}, Модель: {car.Model}, Цвет: {car.Color}, Цена за минуту: {car.RentPricePerMinute}";
+        }
+        else
+        {
+            transportInfo = "Неизвестный тип транспорта";
+        }
+
+        return $"ФИО клиента: {ClientFIO}, {transportInfo}, Время аренды: {RentalTimeMinutes} мин.";
+    }
+
+    public double CalculateTotalCost()
+    {
+        return RentalTimeMinutes * Transport.RentPricePerMinute;
+    }
+}
